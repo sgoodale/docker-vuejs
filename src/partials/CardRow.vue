8180ab3b6a8d76fc4a2a06305592f40004f6439a
stephgiftbit <steph@giftbit.com>
2021-02-14 15:58:52 -0800

    first commit of sample vue project

---
+++ src/partials/CardRow.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-row class="pl-3 pr-3">
+
+        <v-col
+                v-for="n in Number(numCards)"
+                :key="n"
+        >
+
+            <slot :name="'card-' + n"/>
+
+        </v-col>
+
+    </v-row>
+</template>
+
+<script>
+    export default {
+        name: 'CardRow',
+        components: {},
+        data: () => ({
+            //
+        }),
+        props: ['numCards']
+    }
+</script>
